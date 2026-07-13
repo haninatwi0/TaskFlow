e7ccbcf9198fc78533f7fa80b58b79f7868addbf
Hanin <haninatwi0@gmail.com>
2026-07-13 19:55:03 -0300

    Add custom 404 error page

--- app.py
+++ app.py
@@ -192,5 +192,14 @@ with app.app_context():
     db.create_all()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500.html"), 500
+
 if __name__ == "__main__":
     app.run(debug=True)

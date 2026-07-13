e54525dd2f489bed169b33f7472cb2397e134abd
Hanin <haninatwi0@gmail.com>
2026-07-13 12:59:26 -0300

    Connect Flask with SQLite database

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from models.user import db
 
 app = Flask(__name__)
+
+# Database configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///taskflow.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Connect database with Flask
+db.init_app(app)
+
 
 @app.route("/")
 def home():
@@ -15,6 +24,11 @@ def login():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+
+# Create database tables
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == "__main__":
